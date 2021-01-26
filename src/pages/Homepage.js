1999b5dfb579155e1e3495b94470079b56b40c2e
muahmmed-naga <muhammed.ali.naga@gmail.com>
2021-01-26 23:56:49 +0200

    Remove homepage landing to convert it to homepage carousel

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,12 +1,11 @@
 import React from 'react';
 
 // Components
-import HomePageLanding from '../components/homepage-landing/homepage-landing.comp';
 
 const Homepage = () => {
   return (
     <div className="home-page-wrapper">
-      <HomePageLanding />
+      <span>Home Page</span>
     </div>
   );
 };
