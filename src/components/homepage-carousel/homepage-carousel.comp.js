b13d9b2d7c43d39148bca650e22a7a8ea35ae485
muahmmed-naga <muhammed.ali.naga@gmail.com>
2021-01-28 01:55:04 +0200

    Complete home page carousel with make it responsive

--- src/components/homepage-carousel/homepage-carousel.comp.js
+++ src/components/homepage-carousel/homepage-carousel.comp.js
@@ -5,6 +5,8 @@ import AwesomeSlider from 'react-awesome-slider';
 
 // Assets
 import SlideImg1 from '../../assets/images/slider-1.png';
+import SlideImg2 from '../../assets/images/slider-2.png';
+import SlideImg3 from '../../assets/images/slider-3.png';
 
 // Styles
 import './homepage-carousel.styles.scss';
@@ -18,9 +20,7 @@ const HomepageCarousel = () => {
       <AwesomeSlider animation="cubeAnimation">
         <div className="item-wrapper">
           <div className="slide-1-caption">
-            <div className="text-1">
-              the new <br /> standard{' '}
-            </div>
+            <div className="text-1">the new standard </div>
             <div className="text-2">UNDER FAVORABLE SMARTWATCHES</div>
             <div className="cation bold-false">
               <span className="title-price">From</span>
@@ -34,15 +34,54 @@ const HomepageCarousel = () => {
               start buying
             </Link>
           </div>
-          <img src={SlideImg1} alt="" />
+          <div className="img-container">
+            <img src={SlideImg2} alt="" />
+          </div>
         </div>
+
         <div className="item-wrapper">
-          <span>hello 2</span>
-          <img src={SlideImg1} alt="" />
+          <div className="slide-1-caption">
+            <div className="text-1">
+              TIMEPIECES THAT <br /> MAKE A STATEMENT <br /> UP TO{' '}
+            </div>
+            <div
+              className="text-2"
+              style={{
+                fontSize: '3.5rem',
+                fontWeight: '700',
+                color: '#000000ab',
+              }}
+            >
+              52%
+            </div>
+            <Link to="/shop" className="start-buying">
+              start buying
+            </Link>
+          </div>
+          <div className="img-container">
+            <img src={SlideImg1} alt="" />
+          </div>
         </div>
+
         <div className="item-wrapper">
-          <span>hello3</span>
-          <img src={SlideImg1} alt="" />
+          <div className="slide-1-caption">
+            <div className="text-1">the new standard </div>
+            <div className="text-2">UNDER FAVORABLE SMARTWATCHES</div>
+            <div className="cation bold-false">
+              <span className="title-price">From</span>
+              <p>
+                {' '}
+                <span className="small-size">$</span>749
+                <span className="small-size">99</span>
+              </p>
+            </div>
+            <Link to="/shop" className="start-buying">
+              start buying
+            </Link>
+          </div>
+          <div className="img-container">
+            <img src={SlideImg3} alt="" />
+          </div>
         </div>
       </AwesomeSlider>
     </div>
