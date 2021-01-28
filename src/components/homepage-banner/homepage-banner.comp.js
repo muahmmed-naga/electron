b0412308bf6cd1795ef34914bbee1344b810697a
muahmmed-naga <muhammed.ali.naga@gmail.com>
2021-01-28 02:17:28 +0200

    Restyle home page carousel

---
+++ src/components/homepage-banner/homepage-banner.comp.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HomepageBanner = () => {
+  return (
+    <div>
+      <span>Home page banner wrapper</span>
+    </div>
+  );
+};
+
+export default HomepageBanner;
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -3,14 +3,16 @@ import React from 'react';
 // Components
 import HomepageCarousel from '../components/homepage-carousel/homepage-carousel.comp';
 import AppMainNavigator from '../components/app-main-navigator/app-main-navigator.comp';
+import HomepageBanner from '../components/homepage-banner/homepage-banner.comp';
 
 // Styles
 
 const Homepage = () => {
   return (
-    <div className="home-page-wrapper" style={{ position: 'relative' }}>
+    <div className="home-page-wrapper">
       <AppMainNavigator />
       <HomepageCarousel />
+      <HomepageBanner />
     </div>
   );
 };
