147353df7adc90df17fe707c5b231bb15fc12d31
muhammad-naga <muhammad.naga.ali@gmail.com>
2021-03-13 22:49:25 +0200

    New arrivals

--- src/components/new-arrivals/new-arrivals.comp.js
+++ src/components/new-arrivals/new-arrivals.comp.js
@@ -1,69 +1,31 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-
 // Components
-import AliceCarousel from 'react-alice-carousel';
-import LoadingSpinner from '../loading-spinner/loading-spinner.comp';
+import AliceCarousel from 'react-alice-carousel'
 
 // Styles
-import 'react-alice-carousel/lib/alice-carousel.css';
-import './new-arrivals.styles.scss';
-import { Link } from 'react-router-dom';
-import { IoEyeOutline } from 'react-icons/io5';
-import { IoMdHeartEmpty } from 'react-icons/io';
+import 'react-alice-carousel/lib/alice-carousel.css'
+import './new-arrivals.styles.scss'
+import TinyProduct from '../tiny-product/tiny-product.comp'
 
 const NewArrivals = () => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    setIsLoading(true);
-    axios
-      .get('/api/v1/categories/new-arrivals')
-      .then((res) => setData(res.data.data.new_arrivals));
-    setTimeout(() => setIsLoading(false), 300);
-  }, []);
-
   const responsive = {
     0: { items: 2 },
     768: { items: 3 },
     992: { items: 4 },
     1024: { items: 6 },
-  };
+  }
 
-  const items = data.map(({ id, name, img, price, category }) => (
-    <div className="tiny-product-wrapper">
-      <div className="category">{category}</div>
-      <Link
-        to={`/categories/new-arrivals/product/${id}`}
-        className="product-name"
-      >
-        {name}
-      </Link>
-      <Link
-        to={`/categories/new-arrivals/product/${id}`}
-        className="img-wrapper"
-      >
-        <img src={img} alt="product" />
-      </Link>
-
-      <div className="price">
-        <div className="new">${price}</div>
-        <del>$300.00</del>
-      </div>
-
-      <div className="options flex-align-center">
-        <div className="view flex-align-center">
-          <IoEyeOutline />
-          <span>view</span>
-        </div>
-        <div className="wishlist flex-align-center">
-          <IoMdHeartEmpty />
-          <span>wishlist</span>
-        </div>
-      </div>
-    </div>
-  ));
+  const items = [
+    <TinyProduct productImg="https://cdn.shopify.com/s/files/1/0066/4322/0562/products/GoldPhone_bd50e26e-6236-4648-88db-d0b8a94a1bde_540x.jpg?v=1605600708" />,
+    <TinyProduct productImg="https://cdn.shopify.com/s/files/1/0066/4322/0562/products/GoldPhone_bd50e26e-6236-4648-88db-d0b8a94a1bde_540x.jpg?v=1605600708" />,
+    <TinyProduct productImg="https://cdn.shopify.com/s/files/1/0066/4322/0562/products/GoldPhone_bd50e26e-6236-4648-88db-d0b8a94a1bde_540x.jpg?v=1605600708" />,
+    <TinyProduct productImg="https://cdn.shopify.com/s/files/1/0066/4322/0562/products/GoldPhone_bd50e26e-6236-4648-88db-d0b8a94a1bde_540x.jpg?v=1605600708" />,
+    <TinyProduct productImg="https://cdn.shopify.com/s/files/1/0066/4322/0562/products/GoldPhone_bd50e26e-6236-4648-88db-d0b8a94a1bde_540x.jpg?v=1605600708" />,
+    <TinyProduct productImg="https://cdn.shopify.com/s/files/1/0066/4322/0562/products/GoldPhone_bd50e26e-6236-4648-88db-d0b8a94a1bde_540x.jpg?v=1605600708" />,
+    <TinyProduct productImg="https://cdn.shopify.com/s/files/1/0066/4322/0562/products/GoldPhone_bd50e26e-6236-4648-88db-d0b8a94a1bde_540x.jpg?v=1605600708" />,
+    <TinyProduct productImg="https://cdn.shopify.com/s/files/1/0066/4322/0562/products/GoldPhone_bd50e26e-6236-4648-88db-d0b8a94a1bde_540x.jpg?v=1605600708" />,
+    <TinyProduct productImg="https://cdn.shopify.com/s/files/1/0066/4322/0562/products/GoldPhone_bd50e26e-6236-4648-88db-d0b8a94a1bde_540x.jpg?v=1605600708" />,
+    <TinyProduct productImg="https://cdn.shopify.com/s/files/1/0066/4322/0562/products/GoldPhone_bd50e26e-6236-4648-88db-d0b8a94a1bde_540x.jpg?v=1605600708" />,
+  ]
 
   return (
     <div className="custom-container new-arrivals-wrapper m-top-50">
@@ -72,21 +34,17 @@ const NewArrivals = () => {
         <div className="carousel-navigator"></div>
       </div>
 
-      {isLoading ? (
-        <LoadingSpinner />
-      ) : (
-        <AliceCarousel
-          mouseTracking
-          items={items}
-          responsive={responsive}
-          disableDotsControls
-          autoPlay
-          autoPlayInterval={4000}
-          infinite
-        />
-      )}
+      <AliceCarousel
+        mouseTracking
+        items={items}
+        responsive={responsive}
+        disableDotsControls
+        autoPlay
+        autoPlayInterval={4000}
+        infinite
+      />
     </div>
-  );
-};
+  )
+}
 
-export default NewArrivals;
+export default NewArrivals
