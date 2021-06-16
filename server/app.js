a6a4bebb42a354551d3237a06efd8ad9d41b4ac8
muhammad-naga <muhammad.naga.ali@gmail.com>
2021-06-16 15:49:04 +0200

    Prepare for deployment

--- server/app.js
+++ server/app.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import cors from "cors";
 import morgan from "morgan";
@@ -22,8 +23,20 @@ app.use("/api/v1/products", productRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/orders", orderRouter);
 
+const __dirname = path.resolve();
+app.use("/uploads", express.static(path.join(__dirname, "/client/uploads")));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/client/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 // Handler
 app.use(errorPath);
 app.use(errorHandler);
 
 export default app;
+
+// Up to next Update user Profile (5)
